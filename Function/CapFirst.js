3d19d30dff26394a2ccc6523815cec5fbb224154
lehuuphuc2349 <57623623+lehuuphuc2349@users.noreply.github.com>
2020-05-18 09:31:09 +0700

    commit

---
+++ Function/CapFirst.js
@@ -0,0 +1,19 @@
+// const CapFirst = (string) =>
+//   string
+//     .split(" ")
+//     .map((v) => v[0].toUpperCase())
+//     .join("");
+// //  .reduce((v, i = 0) => v[i].toUpperCase(), {})
+// //  .join(" ");
+
+// console.log(CapFirst("phuc dep trai"));
+
+const upperCase = (string) => {
+  var array = string.split(" ");
+  var newArray = [];
+  for (var x = 0; x < array.length; x++) {
+    newArray.push(array[x].charAt(0).toUpperCase() + array[x].slice(1));
+  }
+  return newArray.join(" ");
+};
+console.log(upperCase("phuc dep trai"));
