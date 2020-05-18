e6295761b8301c866bfcfef1db46fc8921a1602c
lehuuphuc2349 <57623623+lehuuphuc2349@users.noreply.github.com>
2020-05-18 08:47:10 +0700

    commit

---
+++ Function/Allcombinations.js
@@ -0,0 +1,22 @@
+function allCombinations(string) {
+  var array1 = [];
+  for (var x = 0, y = 1; x < string.length; x++, y++) {
+    array1[x] = string.substring(x, y);
+  }
+  var combination = [];
+  var temp = "";
+  var slent = Math.pow(2, array1.length);
+  for (var i = 0; i < slent; i++) {
+    temp = "";
+    for (var j = 0; j < array1.length; j++) {
+      if (i & Math.pow(2, j)) {
+        temp += array1[j];
+      }
+    }
+    if (temp !== "") {
+      combination.push(temp);
+    }
+  }
+  console.log(combination.join("\n"));
+}
+allCombinations("phuc");
