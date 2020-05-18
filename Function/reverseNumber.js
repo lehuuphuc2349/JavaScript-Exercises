e25eccda983cf8c01ec26da832ee3b020825dd44
lehuuphuc2349 <57623623+lehuuphuc2349@users.noreply.github.com>
2020-05-18 08:17:17 +0700

    commit

--- Function/reverseNumber.js
+++ Function/reverseNumber.js
@@ -3,3 +3,9 @@ const reverseNumber = (number) =>
 console.log(reverseNumber(123));
 console.log(reverseNumber(32243));
 console.log(reverseNumber(9929));
+
+const reverseNumber1 = (number) => {
+  number = number + "";
+  return number.split("").reverse().join("");
+};
+console.log(reverseNumber1(23232));
