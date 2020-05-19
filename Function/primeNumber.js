9c45fccc6e2092cdc9249183c5b802b2ff174a7e
lehuuphuc2349 <57623623+lehuuphuc2349@users.noreply.github.com>
2020-05-19 08:05:41 +0700

    commit

---
+++ Function/primeNumber.js
@@ -0,0 +1,16 @@
+const testPrime = (n) => {
+  if (n === 1) {
+    return false;
+  } else if (n === 2) {
+    return true;
+  } else {
+    for (var x = 2; x < n; x++) {
+      if (n % x === 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+console.log(testPrime(3));
+console.log(testPrime(37));
