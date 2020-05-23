e3fd541517fe117bf20267e97c3dd54152483ad1
lehuuphuc2349 <57623623+lehuuphuc2349@users.noreply.github.com>
2020-05-23 09:53:16 +0700

    commit

---
+++ Math/part2/converse.js
@@ -0,0 +1,8 @@
+const converseNumber = (number) => Math.sign(Math.abs(number));
+console.log(converseNumber(1));
+console.log(converseNumber(-2));
+console.log(converseNumber(-3));
+console.log(converseNumber(-4));
+
+const posToNega = (number) => -number;
+console.log(posToNega(1));
