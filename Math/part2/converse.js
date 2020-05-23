96cc9efbe40c941d525a2652aaf970eaae40236b
lehuuphuc2349 <57623623+lehuuphuc2349@users.noreply.github.com>
2020-05-23 09:57:02 +0700

    commit

--- Math/part2/converse.js
+++ Math/part2/converse.js
@@ -6,3 +6,7 @@ console.log(converseNumber(-4));
 
 const posToNega = (number) => -number;
 console.log(posToNega(1));
+
+var number = 17;
+var result = Math.sqrt(number);
+console.log(Math.round(result));
---
+++ Math/part2/roundSqrt.js
@@ -0,0 +1,6 @@
+const roundSqrt = (number) => Math.round(Math.sqrt(number));
+console.log(roundSqrt(12));
+console.log(roundSqrt(22));
+console.log(roundSqrt(42));
+console.log(roundSqrt(20));
+console.log(roundSqrt(18));
