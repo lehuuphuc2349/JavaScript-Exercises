37433ab176d8fd9ec328af44ed0086817d281831
lehuuphuc2349 <57623623+lehuuphuc2349@users.noreply.github.com>
2020-06-01 09:53:29 +0700

    add

--- dateTime/part2/numericDay.js
+++ dateTime/part2/numericDay.js
@@ -1,2 +1,14 @@
-var day = new Date(2015, 10, 1);
-console.log(day.getDay());
+function getNumericday(date) {
+  var list = [7, 1, 2, 3, 4, 5, 6];
+  var day = new Date(date).getDay();
+  return list[day];
+}
+console.log(getNumericday(new Date(2015, 10, 1)));
+console.log(getNumericday(new Date()));
+
+const convertNumber = (date) => {
+  var day = new Date(date);
+  return day.getDay() === 0 ? 7 : day.getDay();
+};
+console.log(convertNumber(new Date()));
+console.log(convertNumber(new Date(2015, 10, 1)));
